7f9255ff562c92b08689d1a68f9a38f408a4b622
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-11-28 13:08:18 +0530

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -5,10 +5,9 @@ sudo apt-get install -y nodejs git
 sudo npm install -g yarn pm2
 
 cd /opt
-git clone https://github.com/strapi/strapi-app-template.git strapi
+git clone https://github.com/Kabilan2370/strapi-aws-production.git strapi
 cd strapi
 
-yarn install
 
 # Environment variables for DB
 echo "
@@ -18,8 +17,12 @@ DATABASE_PORT=5432
 DATABASE_NAME=${db_name}
 DATABASE_USERNAME=${db_user}
 DATABASE_PASSWORD=${db_password}
+AWS_REGION=us-east-1
+AWS_S3_BUCKET=${s3_bucket}
 " > .env
 
-yarn build
-pm2 start yarn --name strapi -- start
+npm install
+npm run build
+
+pm2 start npm --name "strapi" -- start
 pm2 save
