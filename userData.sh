d50d05aa8b5ff01e5cbff1173b06fb3ecefb6c78
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-11-28 17:26:43 +0530

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+# Make swap permanent
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
 sudo apt-get update -y
 
 sudo curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
