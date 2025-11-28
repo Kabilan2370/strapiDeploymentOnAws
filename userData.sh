f973a337a9f3394f09d6ea3e2bd39968b3a00803
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-11-28 16:20:35 +0530

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
+sudo apt-get update -y
 
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+sudo curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 sudo apt-get install -y nodejs git
 sudo npm install -g yarn pm2
 
 cd /opt
-git clone https://github.com/Kabilan2370/strapi-aws-production.git strapi
-cd strapi
+sudo git clone https://github.com/Kabilan2370/strapi-aws-production.git strapi
+sudo chown -R ubuntu:ubuntu /opt/strapi
+cd /opt/strapi
 
 
 # Environment variables for DB
-echo "
+sudo bash -c "cat <<EOF > .env
 DATABASE_CLIENT=postgres
 DATABASE_HOST=${db_host}
 DATABASE_PORT=5432
-DATABASE_NAME=${db_name}
 DATABASE_USERNAME=${db_user}
 DATABASE_PASSWORD=${db_password}
 AWS_REGION=us-east-1
 AWS_S3_BUCKET=${s3_bucket}
-" > .env
+EOF"
 
-npm install
-npm run build
+# Install and build Strapi
+sudo -u ubuntu npm install
+sudo -u ubuntu npm run build
 
-pm2 start npm --name "strapi" -- start
-pm2 save
+# Start Strapi with PM2
+sudo pm2 start npm --name "strapi" -- start
+sudo pm2 save
+sudo pm2 startup systemd -u ubuntu --hp /home/ubuntu
