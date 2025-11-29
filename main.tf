f21e2680b5fba4e6d6509dbd20643b7ab49b4d07
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-11-29 23:13:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -171,7 +171,7 @@ resource "aws_security_group" "private_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    ciddr_blocks  = ["10.0.2.0/24"]
+    cidr_blocks  = ["10.0.2.0/24"]
   }
 
   tags = {
